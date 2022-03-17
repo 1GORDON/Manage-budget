7f6ef05e780da36e407c81b2dbc3379ecad92ae1
Gordon Tinyefuza <gordon.tinyefuza@gmail.com>
2022-03-17 17:14:51 +0300

    Update entities view

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -4,33 +4,33 @@ class EntitiesController < ActionController::Base
     @relation = Relation.new(entity: @entity)
   end
 
-  # def create
-  #   entity = current_user.entities.new(entity_params)
-  #   respond_to do |format|
-  #     format.html do
-  #       if entity.save
-  #         relation = Relation.create(group_id: group_params, entity_id: entity.id)
-  #         redirect_to group_path(relation.group_id)
-  #       else
-  #         render :new
-  #       end
-  #     end
-  #   end
-  # end
+  def create
+    entity = current_user.entities.new(entity_params)
+    respond_to do |format|
+      format.html do
+        if entity.save
+          relation = Relation.create(group_id: group_params, entity_id: entity.id)
+          redirect_to group_path(relation.group_id)
+        else
+          render :new
+        end
+      end
+    end
+  end
 
-  # def destroy
-  #   @entity = current_user.entities.find(params[:id])
-  #   @entity.destroy
-  #   redirect_to groups_path
-  # end
+  def destroy
+    @entity = current_user.entities.find(params[:id])
+    @entity.destroy
+    redirect_to groups_path
+  end
 
-  # private
+  private
 
-  # def entity_params
-  #   params.require(:entity).permit(:name, :amount)
-  # end
+  def entity_params
+    params.require(:entity).permit(:name, :amount)
+  end
 
-  # def group_params
-  #   params[:entity][:relation][:group_id]
-  # end
+  def group_params
+    params[:entity][:relation][:group_id]
+  end
 end
