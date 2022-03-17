f549f27bc197810543bcdc2c7d13734d7904ba3f
Gordon Tinyefuza <gordon.tinyefuza@gmail.com>
2022-03-17 11:37:54 +0300

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root 'users#index'
+  resources :users, only: [:index]
+  resources :catagories, only: [:index]
+  resources :transactions, only: [:index]
+  resources :manage_budget, only: [:index]#splash screen
 end
