43ff237e1368229aec828d6e85d7803965c00d2f
Gordon Tinyefuza <gordon.tinyefuza@gmail.com>
2022-03-17 12:24:19 +0300

    Update user view

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root 'users#index'
+  # root 'users#index'
   resources :users, only: [:index]
   resources :catagories, only: [:index]
   resources :transactions, only: [:index]
