0465603c238708226cffe459e8a43c6bd4840371
Gordon Tinyefuza <gordon.tinyefuza@gmail.com>
2022-03-17 17:42:10 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
-  devise_scope :users do
+  devise_scope :user do
+    authenticated  do
+      root to: 'groups#index'
+   end
+
+  unauthenticated do
+      root to: 'splash#index', as: 'unauthenticated_root'
+  end
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-  root 'splash#index'
-  resources :users, only: [:index]
-  resources :catagories, only: [:index]
-  resources :transactions, only: [:index]
-  resources :splash, only: [:index] # splash screen or home page
+  resources :splash, only: [:index]
+  resources :entities, only: [:new, :create, :destroy] do
+    resources :relations, only: [:create]
+  end
+  resources :groups, only: [:index, :show, :new, :create, :destroy]
 end
