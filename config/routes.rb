6abfb61ecd400888e68b11679643e72778c6cecc
Gordon Tinyefuza <gordon.tinyefuza@gmail.com>
2022-03-18 15:01:13 +0300

    Fix Linters

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
   devise_scope :user do
-    authenticated  do
+    authenticated do
       root to: 'groups#index'
-   end
+    end
 
-  unauthenticated do
+    unauthenticated do
       root to: 'splash#index', as: 'unauthenticated_root'
-  end
+    end
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
 
   resources :splash, only: [:index]
-  resources :entities, only: [:new, :create, :destroy] do
+  resources :entities, only: %i[new create destroy] do
     resources :relations, only: [:create]
   end
-  resources :groups, only: [:index, :show, :new, :create, :destroy]
-
+  resources :groups, only: %i[index show new create destroy]
 end
