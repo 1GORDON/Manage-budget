f92ebba093c10eacda4a28c3b1b3277cf169250d
Gordon Tinyefuza <gordon.tinyefuza@gmail.com>
2022-03-18 17:29:33 +0300

    Fix Linters

--- db/migrate/20220318125324_create_relations.rb
+++ db/migrate/20220318125324_create_relations.rb
@@ -1,8 +1,5 @@
 class CreateRelations < ActiveRecord::Migration[7.0]
   def change
-    create_table :relations do |t|
-
-      t.timestamps
-    end
+    create_table :relations, &:timestamps
   end
 end
--- test/models/relation_test.rb
+++ test/models/relation_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class RelationTest < ActiveSupport::TestCase
   # test "the truth" do
